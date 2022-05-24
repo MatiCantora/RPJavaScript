8bf054b3108e38e119889d845b758b0a90fe9435
Matías Cantora <cantoramatias@gmail.com>
2022-05-24 17:24:08 -0300

    Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from "react"
+import { Routes, Route } from "react-router-dom"
+import "./App.css"
+
+import Home from "./components/Home"
+import About from "./components/About"
+import Upload from "./components/Upload"
+
+import NavBar from "./components/NavBar"
+// import Footer from "./components/Footer"
 
 function App() {
 	return (
-		<div>
-			<h1 className="text-3xl font-bold underline">Hello world!</h1>
+		<div className="App">
+			<NavBar />
+			<Routes>
+				<Route exact path="/" element={<Home />} />
+				<Route exact path="/about" element={<About />} />
+				<Route exact path="/upload" element={<Upload />} />
+			</Routes>
 		</div>
 	)
 }
